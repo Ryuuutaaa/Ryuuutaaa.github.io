cd8163d97414885289ff9c1c4c5f99d34c3c965c
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-05-04 22:35:53 +0800

    add page skill in about

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 // typing animation
 var typed = new Typed(".typing", {
-  strings: ["", "Web developer", "Orang gabut"],
+  strings: ["", "Web developer", "Random People", "Junior Collage"],
   typeSpeed: 100,
   BackSpeed: 60,
   loop: true,
